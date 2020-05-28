283c113f3580008bdd4aeff2c103d0ea856a950f
yuna kim <yuna_k@regularbold.co.kr>
2020-05-28 15:00:59 +0900

    gulp..

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,57 +1,49 @@
-'use strict';
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import eslint from 'gulp-eslint'
+import base64 from 'gulp-base64'
+import sass from 'gulp-sass'
+import autoprefixer from 'gulp-autoprefixer'
+import fileinclude from 'gulp-file-include'
+import del from 'del'
+import browserSync from 'browser-sync'
+import browserify from 'browserify'
+import source from 'vinyl-source-stream'
+import buffer from 'vinyl-buffer'
+import babelify from 'babelify'
+import uglify from 'gulp-uglify'
 
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import eslint from 'gulp-eslint';
-import base64 from 'gulp-base64';
-import sass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
-import fileinclude from 'gulp-file-include';
-import concat from 'gulp-concat';
-import del from 'del';
-import browserSync from 'browser-sync';
-import browserify from 'browserify';
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
-import babelify from 'babelify';
-
-const server = browserSync.create();
-const onError = (err) => console.log(err);
-const clean = () => del(['dist']);
-
+const server = browserSync.create()
+const clean = () => {return del(['dist'])}
 
 const scssOptions = { // Sass compile option
-  outputStyle: "expanded",
-  indentType: "space",
+  outputStyle: 'expanded',
+  indentType: 'space',
   indentWidth: 2,
   precision: 6,
   sourceComments: false
-};
+}
 
-const src = 'src';
-const dist = 'dist';
+const src = 'src'
+const dist = 'dist'
 const paths = {
   js: [`${src}/**/*.js`, `!${src}/js/plugins/*.js`],
   plugins: `${src}/js/plugins/index.js`,
   scss: `${src}/**/*.scss`,
   html: `${src}/**/*.html`,
   image : `${src}/**/*.{png,jpg,jpeg,gif,svg,ico,mp4}`,
-  font : `${src}/**/*.{ttf,otf,woff,woff2,eot,svg}`,
-};
+  font : `${src}/**/*.{ttf,otf,woff,woff2,eot,svg}`
+}
 
 const htmlIncludeOptions = {
   prefix: '@@',
   basepath: '@file',
-  indent: true,
-  context: {
-    adminmenu: '',
-    menu: '',
-  }
-};
+  indent: true
+}
 
 function style() {
   return gulp.src(paths.scss, {sourcemaps: true})
-    .pipe(sass(scssOptions))
+    .pipe(sass(scssOptions).on('error', sass.logError))
     .pipe(base64({
         extensions: ['svg', 'png', /\.jpg#datauri$/i],
         maxImageSize: 8192,
@@ -59,34 +51,26 @@ function style() {
     }))
     .pipe(autoprefixer())
     .pipe(gulp.dest(dist, {sourcemaps: '/maps'}))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 }
 
 function htmlInclude() {
   return gulp.src([paths.html, '!src/html/inc/*.html'])
     .pipe(fileinclude(htmlIncludeOptions))
     .pipe(gulp.dest(dist))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 }
-
-// function plugins() {
-//   return gulp.src(paths.plugins, { sourcemaps: true })
-//     .pipe(babel())
-//     .pipe(eslint.format())
-//     .pipe(concat('plugins.js'))
-//     .pipe(gulp.dest(dist));
-// }
 
 function plugins() {
   return browserify(paths.plugins)
     .transform(babelify,{
       presets : ['@babel/preset-env']
     })
-    // .pipe(babel())
     .bundle()
-    .pipe(source('./js/plugins.js')) //vinyl object 로 변환
-    .pipe(buffer()) //buffered vinyl object 로 변환
-    .pipe(gulp.dest(dist));
+    .pipe(source('./js/plugins.js')) // vinyl object 로 변환
+    .pipe(buffer()) // buffered vinyl object 로 변환
+    .pipe(uglify({toplevel:true}))
+    .pipe(gulp.dest(dist))
 }
 
 function scripts() {
@@ -94,45 +78,38 @@ function scripts() {
     .pipe(babel({
       presets : ['@babel/preset-env']
     }))
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest(dist))
 }
 
 function lintBase() {
-  return gulp.src(paths.js)
+  return gulp.src(paths.plugins)
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-}
-
-function copyFonts() {
-
+    .pipe(eslint.failAfterError())
 }
 
 function copyImage() {
   return gulp.src(paths.image, {since: gulp.lastRun(copyImage)})
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest(dist))
 }
 
 function watchFiles(done) {
   server.init({
-    server: "./dist/",
+    server: './dist/',
     port: 5000,
     open: false
-  });
-  done();
-  gulp.watch(paths.scss, style);
-  gulp.watch(paths.html, htmlInclude);
-  gulp.watch(paths.plugins, plugins);
-  gulp.watch(paths.js, scripts);
-  gulp.watch(paths.image, copyImage);
+  })
+  done()
+  gulp.watch(paths.scss, style)
+  gulp.watch(paths.html, htmlInclude)
+  gulp.watch(paths.plugins, plugins)
+  gulp.watch(paths.js, scripts)
+  gulp.watch(paths.image, copyImage)
 }
 
+const watch = gulp.parallel(watchFiles, lintBase)
+const build = gulp.series(clean, gulp.parallel(style, plugins, scripts, htmlInclude, copyImage))
 
-
-const watch = gulp.parallel(watchFiles, lintBase);
-const build = gulp.series(clean, gulp.parallel(style, plugins, scripts, htmlInclude, copyImage));
-
-
-exports.watch = watch;
-exports.build = build;
-exports.default = build;
+exports.watch = watch
+exports.build = build
+exports.default = build
