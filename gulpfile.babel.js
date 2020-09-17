47e35a77383c3718b3da5482e0f7d41fac084706
yuna kim <yuna_k@regularbold.co.kr>
2020-09-17 17:44:31 +0900

    gulp setting 중

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,7 @@ import browserify from 'browserify'
 import source from 'vinyl-source-stream'
 import buffer from 'vinyl-buffer'
 import babelify from 'babelify'
+import bro from "gulp-bro"
 import uglify from 'gulp-uglify'
 
 const server = browserSync.create()
@@ -26,7 +27,8 @@ const scssOptions = { // Sass compile option
 const src = 'src'
 const dist = 'dist'
 const paths = {
-  js: [`${src}/**/*.js`],
+  js: [`${src}/**/*.js`, `!${src}/js/libs/*.js`],
+  libs: `${src}/js/libs/*.js`,
   scss: `${src}/**/*.scss`,
   html: `${src}/**/*.html`,
   image : `${src}/**/*.{png,jpg,jpeg,gif,svg,ico,mp4}`,
@@ -43,9 +45,9 @@ function style() {
   return gulp.src(paths.scss, {sourcemaps: true})
     .pipe(sass(scssOptions).on('error', sass.logError))
     .pipe(base64({
-        extensions: ['svg', 'png', /\.jpg#datauri$/i],
-        maxImageSize: 8192,
-        debug: true
+      extensions: ['svg', 'png', /\.jpg#datauri$/i],
+      maxImageSize: 1024,
+      debug: true
     }))
     .pipe(autoprefixer())
     .pipe(gulp.dest(dist, {sourcemaps: '/maps'}))
@@ -59,16 +61,46 @@ function htmlInclude() {
     .pipe(browserSync.stream())
 }
 
+// function plugins() {
+//   return browserify(paths.plugins)
+//     .transform(babelify,{
+//       presets : ['@babel/preset-env']
+//     })
+//     .bundle()
+//     .pipe(source('./js/plugins.js')) // vinyl object 로 변환
+//     .pipe(buffer()) // buffered vinyl object 로 변환
+//     .pipe(uglify({toplevel:true}))
+//     .pipe(gulp.dest(dist))
+// }
+
+function libs() {
+  return gulp.src(paths.libs, { sourcemaps: true })
+    .pipe(gulp.dest(dist+'/js/libs/'))
+}
+
 function scripts() {
+  // return gulp.src(paths.js, { sourcemaps: true })
+  //   .pipe(babel({
+  //     presets : ['@babel/env']
+  //   }))
+  //   .pipe(gulp.dest(dist))
   return gulp.src(paths.js, { sourcemaps: true })
-    .pipe(babel({
-      presets : ['@babel/preset-env']
+    .pipe(bro({
+      transform: [
+        babelify.configure({ presets: ['@babel/preset-env'] }),
+        //[ 'uglifyify', { global: true } ]
+      ]
     }))
     .pipe(gulp.dest(dist))
 }
 
 function copyImage() {
   return gulp.src(paths.image, {since: gulp.lastRun(copyImage)})
+    .pipe(gulp.dest(dist))
+}
+
+function copyFonts() {
+  return gulp.src(paths.font, {since: gulp.lastRun(copyFonts)})
     .pipe(gulp.dest(dist))
 }
 
@@ -82,12 +114,14 @@ function watchFiles(done) {
   gulp.watch(paths.scss, style)
   gulp.watch(paths.html, htmlInclude)
   // gulp.watch(paths.plugins, plugins)
+  gulp.watch(paths.libs, libs)
   gulp.watch(paths.js, scripts)
   gulp.watch(paths.image, copyImage)
+  gulp.watch(paths.font, copyFonts)
 }
 
 const watch = gulp.parallel(watchFiles)
-const build = gulp.series(clean, gulp.parallel(style, scripts, htmlInclude, copyImage))
+const build = gulp.series(clean, gulp.parallel(style, scripts, libs, htmlInclude, copyImage, copyFonts))
 
 exports.watch = watch
 exports.build = build
