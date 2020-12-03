63065b7cfe690762f02d06e79f52c438cbdc0d26
ryuzuka <ryuzuka@gmail.com>
2020-12-03 12:31:23 +0900

    plugin execution app -> common

--- src/js/plugins/blockBodyScroll.js
+++ src/js/plugins/blockBodyScroll.js
@@ -23,7 +23,7 @@
       this.prevScroll = window.scrollY || window.pageYOffset
       let style = 'overflow: hidden; width: 100%; min-width: 100%; height: 100%; min-height: 100%;'
       if (navigator.userAgent.indexOf('Mobi') > -1) {
-        style += `position: fixed; margin-top: ${-1 * this.prevScroll}px;`
+        style += ' ' + `position: fixed; margin-top: ${-1 * this.prevScroll}px;`
       }
       this.$body.attr('style', style)
     }
