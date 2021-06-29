e197ec9781c27fb56d93e28ad36b426944f55972
ryuzuka <ryuzuka@gmail.com>
2021-06-29 16:37:54 +0900

    plugin - manager, execution

--- src/js/common.js
+++ src/js/common.js
@@ -184,33 +184,6 @@
       }
     },
 
-    /** plugin manager */
-    plugin: {
-      add($el, _pluginName, _plugin) {
-        if ($el.attr('applied-plugin')) {
-          return
-        }
-        let pluginId = _pluginName + pluginIndex
-        $el.attr('applied-plugin', pluginId)
-        pluginPool[pluginId] = _plugin
-        pluginIndex++
-      },
-      remove($el) {
-        delete pluginPool[$el.attr('applied-plugin')]
-        $el.removeAttr('applied-plugin')
-      },
-      call($el, _method, _value) {
-        let pluginId = $el.attr('applied-plugin')
-        if (!pluginId) {
-          return
-        }
-        pluginPool[pluginId][_method](_value)
-        if (_method === 'clear') {
-          this.remove($el)
-        }
-      }
-    },
-
     /** easing */
     ease: {
       Quad: {
@@ -254,13 +227,6 @@
         easeInOut: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
       }
     }
-  })
-
-  /** jquery plugins execution */
-  $(() => {
-    $('.js-accordion').accordion()
-    $('.js-dropdown').dropdown()
-    $('.js-tab').tab()
   })
 })(window.jQuery)
 /** ****************************************************************************************************************** */
