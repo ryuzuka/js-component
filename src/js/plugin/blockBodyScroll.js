b733fb452e92f18c562a2d772619d3c7949d8b36
ksg <ryuzuka@regularbold.co.kr>
2023-01-10 11:04:21 +0900

    blockBodyScroll - es6

--- src/js/plugin/blockBodyScroll.js
+++ src/js/plugin/blockBodyScroll.js
@@ -5,7 +5,7 @@
   $.extend({
     blockBodyScroll: function (isBlock) {
       _plugin = _plugin || new BlockBodyScroll()
-      _plugin[isBlock ? 'block' : 'scroll']()
+      _plugin.block(isBlock)
 
       return _plugin
     }
@@ -13,29 +13,14 @@
 
   class BlockBodyScroll {
     constructor () {
-      this.prevScroll = 0
-      this.$body = $('body')
       this.isBlock = false
     }
 
-    block () {
-      if (this.isBlock) return
-      this.isBlock = true
+    block (isBlock) {
+      if (this.isBlock === isBlock) return
+      this.isBlock = isBlock
 
-      this.prevScroll = window.scrollY || window.pageYOffset
-      let style = 'overflow: hidden; width: 100%; height: 100%; min-width: 100%; min-height: 100%;'
-      if ($.utils.isMobile()) {
-        style += ' ' + `position: fixed; margin-top: ${-1 * this.prevScroll}px;`
-      }
-      this.$body.attr('style', style).addClass('block-body-scroll')
-    }
-
-    scroll () {
-      if (!this.isBlock) return
-      this.isBlock = false
-
-      this.$body.removeAttr('style').removeClass('block-body-scroll')
-      $(window).scrollTop(this.prevScroll)
+      document.body.classList[isBlock ? 'add' : 'remove']('block-body-scroll')
     }
   }
 })(window.jQuery)
--- src/js/plugin/preventScroll.js
+++ src/js/plugin/preventScroll.js
@@ -1,6 +1,7 @@
 /** preventScroll.js ****************************************************************************************************** */
 ;($ => {
   let _plugin = null
+
   $.extend({
     preventScroll: function (isPrevent) {
       _plugin = _plugin || new PreventScroll()
@@ -15,10 +16,6 @@
       this.isPrevent = false
     }
 
-    preventEventHandler (e) {
-      e.preventDefault()
-    }
-
     prevent (isPrevent) {
       if (this.isPrevent === isPrevent) return
       this.isPrevent = isPrevent
@@ -26,6 +23,10 @@
       document.body[(isPrevent ? 'add' : 'remove') + 'EventListener']('wheel', this.preventEventHandler, {passive: false})
       document.body[(isPrevent ? 'add' : 'remove') + 'EventListener']('touchmove', this.preventEventHandler, {passive: false})
       document.body.classList[isPrevent ? 'add' : 'remove']('prevent-scroll')
+    }
+
+    preventEventHandler (e) {
+      e.preventDefault()
     }
   }
 })(window.jQuery)
