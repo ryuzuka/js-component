e02428defb94c9f1e2af1fab9c7d008452322a00
ksg <ryuzuka@regularbold.co.kr>
2023-02-01 16:38:54 +0900

    modal 작업중..

--- src/js/plugin/Modal.js
+++ src/js/plugin/Modal.js
@@ -1,36 +1,73 @@
 /** Modal.js ********************************************************************************************************** */
-let _modal = {}
+const PLUGIN_NAME = 'Modal'
 
 Object.assign(Object.prototype, {
-  Modal (options = {}, callback) {
-    let id = this.id
+  Modal (options = {}, callback = function () {}) {
     if (typeof options === 'string') {
-      _modal[id][options](callback)
+      return window.PLUGIN.call(this, options, callback)
     } else {
-      _modal[id] = _modal[id] || new Modal(this, options)
-      _modal[id].open()
+      let plugin = null
+      if (!this.getAttribute('applied-plugin')) {
+        window.PLUGIN.add(this, plugin = new Modal(this, options, callback), PLUGIN_NAME)
+      }
+      return plugin
     }
-    return this
   }
 })
 
 class Modal {
-  constructor (el, options) {
+  constructor (el, options, callback) {
     this.$modal = el
-    this.id = el.id
+    this.$close = el.querySelector('.close')
+    this.$button = el.querySelectorAll('.button-wrap button.btn')
 
+    this.callback = callback
     this.options = Object.assign({
-      classes: '',
+      classes: [], // Array
       dimmed: true,
       clickToClose: false,
       closedFocus: ''
     }, options)
 
+    this.eventHandler = {
+      clickClose: e => {
+        this.close()
+      }
+    }
 
+    if (this.options.dimmed) {
+      this.options.classes.push('dimmed')
+    }
+
+    if (this.options.classes.length > 0) {
+      this.options.classes.forEach(className => {
+        this.$modal.classList.add(className)
+      })
+    }
+
+    this.$close.addEventListener('click', this.eventHandler.clickClose)
+    this.$button.forEach($btn => {
+      $btn.addEventListener('click', this.eventHandler.clickClose)
+    })
   }
 
-  open () {}
+  open () {
+    window.BlockScroll('block')
+    this.$modal.style.display = 'block'
+    this.callback({type: 'open', $modal: this.$modal})
+  }
 
-  close () {}
+  close () {
+    this.callback({type: 'before-close', $modal: this.$modal})
+
+    this.$modal.style.display = 'none'
+    window.BlockScroll('scroll')
+
+    this.callback({type: 'close', $modal: this.$modal})
+  }
+
+  clear () {
+    this.$close.removeEventListener('click', this.eventHandler.clickClose)
+  }
 }
 /** ****************************************************************************************************************** */
