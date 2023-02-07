edcbe3a083036cf10baaf8186ed04933c0666fb0
ksg <ryuzuka@regularbold.co.kr>
2023-02-07 09:25:02 +0900

    modal 완료

--- src/js/plugin/Modal.js
+++ src/js/plugin/Modal.js
@@ -2,15 +2,14 @@
 const PLUGIN_NAME = 'Modal'
 
 Object.assign(Object.prototype, {
-  Modal (options = {}, callback = function () {}) {
+  Modal (options = {}, callback = () => {}) {
     if (typeof options === 'string') {
       return window.PLUGIN.call(this, options, callback)
     } else {
-      let plugin = null
       if (!this.getAttribute('applied-plugin')) {
-        window.PLUGIN.add(this, plugin = new Modal(this, options, callback), PLUGIN_NAME)
+        window.PLUGIN.add(this, new Modal(this, options, callback), PLUGIN_NAME)
       }
-      return plugin
+      return this
     }
   }
 })
@@ -23,63 +22,80 @@ class Modal {
 
     this.callback = callback
     this.options = Object.assign({
-      classes: [], // Array
-      dimmed: true,
-      clickToClose: false,
-      closedFocus: ''
+      classes: '',
+      clickToClose: false
     }, options)
 
-    this.eventHandler = {
-      clickClose: e => {
-        this.close()
-      },
-      clickDimmedClose: e => {
-        if (!this.options.clickToClose) return
+    this.options.classes = this.options.classes.split(' ').filter(className => {
+      if (className !== '' ) {
+        this.$modal.classList.add(className)
+        return className
+      }
+    })
 
+    this.eventHandler = {
+      close: e => this.close(),
+      keydownClose: e => {
+        if (e.keyCode === 27) {
+          this.close()
+        }
+      },
+      clickToClose: e => {
+        if (!this.options.clickToClose) return
         if (e.target.id === this.$modal.id && e.target.className.indexOf('layer') > -1) {
           this.close()
         }
+      },
+      clickButtonClose: e => {
+        let idx = [...e.target.parentElement.children].indexOf(e.target)
+        this.close(idx)
       }
     }
 
-    if (this.options.dimmed) {
-      this.options.classes.push('dimmed')
-    }
-
-    if (this.options.classes.length > 0) {
-      this.options.classes.forEach(className => {
-        this.$modal.classList.add(className)
-      })
-    }
-
-    this.$modal.addEventListener('click', this.eventHandler.clickDimmedClose)
-    this.$close.addEventListener('click', this.eventHandler.clickClose)
+    this.$modal.addEventListener('keydown', this.eventHandler.keydownClose)
+    this.$modal.addEventListener('click', this.eventHandler.clickToClose)
     this.$button.forEach($btn => {
-      $btn.addEventListener('click', e => {
-        let idx = [...e.target.parentElement.children].indexOf(e.target)
-        this.close(idx)
-      })
+      $btn.addEventListener('click', this.eventHandler.clickButtonClose)
     })
+    if (this.$close) {
+      this.$close.addEventListener('click', this.eventHandler.close)
+    }
   }
 
   open () {
+    setTimeout(() => this.$modal.querySelectorAll('button')[0].focus(), 1)
     window.BlockScroll('block')
     this.$modal.style.display = 'block'
+
     this.callback({type: 'open', $modal: this.$modal})
   }
 
   close (idx) {
     let params = idx === undefined ? {} : {closedIndex: idx}
-
     this.callback(Object.assign({type: 'before-close', $modal: this.$modal}, params))
-    this.$modal.style.display = 'none'
+
     window.BlockScroll('scroll')
+    this.$modal.style.display = 'none'
     this.callback(Object.assign({type: 'close', $modal: this.$modal}, params))
+
+    document.querySelector('[aria-controls="' + this.$modal.id + '"]').focus()
   }
 
   clear () {
-    this.$modal.classList.remove('dimmed')
-    this.$close.removeEventListener('click', this.eventHandler.clickClose)
+    this.$modal.removeEventListener('keydown', this.eventHandler.keydownClose)
+    this.$modal.removeEventListener('click', this.eventHandler.clickToClose)
+    this.$button.forEach($btn => {
+      $btn.removeEventListener('click', this.eventHandler.clickButtonClose)
+    })
+    if (this.$close) {
+      this.$close.removeEventListener('click', this.eventHandler.close)
+    }
+
+    this.callback = () => {}
+    this.options = {
+      classes: '',
+      clickToClose: false
+    }
   }
 }
 /** ***************************************************************************************************************** */
