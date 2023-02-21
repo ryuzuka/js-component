a9c1f72ad7f4172c7861dc1048b5f80f0d246666
ksg <ryuzuka@regularbold.co.kr>
2023-02-21 11:27:49 +0900

    extends - Input class(Text, Checkbox, Radio)

--- src/js/plugin/Input.js
+++ src/js/plugin/Input.js
@@ -8,7 +8,10 @@ Object.assign(HTMLElement.prototype, {
     } else {
       let appliedPlugin = this.getAttribute('applied-plugin')
       if (!appliedPlugin || appliedPlugin.indexOf(PLUGIN_NAME) < 0) {
-        window.PLUGIN.add(this, new Input(this, options), PLUGIN_NAME)
+        let type = this.querySelector('input').type
+        if (type === 'text')            window.PLUGIN.add(this, new Text(this, options), PLUGIN_NAME)
+        else if (type === 'checkbox')   window.PLUGIN.add(this, new Checkbox(this, options), PLUGIN_NAME)
+        else if (type === 'radio')      window.PLUGIN.add(this, new Radio(this, options), PLUGIN_NAME)
       }
       return this
     }
@@ -18,76 +21,82 @@ Object.assign(HTMLElement.prototype, {
 class Input {
   constructor (el, options) {
     this.$input = el.querySelector('input')
-    this.type = this.$input.type
     this.options = Object.assign({}, options)
-
-    this.eventHandler = {}
-
-    if (this.type === 'text') {
-      this.text()
-      if ([...this.$input.classList].indexOf('number') > -1) {
-        this.number()
-      } else if ([...this.$input.classList].indexOf('tel') > -1) {
-        this.tel()
-      }
-    } else if (this.type === 'checkbox') {
-      this.checkbox()
-    }
   }
 
-  checkbox () {
-    Object.assign(this.eventHandler, {
-      checkbox: e => {
-        e.target.setAttribute('checked', e.target.checked)
-      }
-    })
+  clear () {}
+}
 
-    this.$input.addEventListener('change', this.eventHandler.checkbox)
-  }
+class Text extends Input {
+  constructor (el, options) {
+    super(el, options)
 
-  text () {
-    Object.assign(this.eventHandler, {
-      focus: e => e.target.classList[e.type === 'focus' ? 'add' : 'remove']('focus')
-    })
-
-    this.$input.addEventListener('focus', this.eventHandler.focus)
-    this.$input.addEventListener('blur', this.eventHandler.focus)
-  }
-
-  number () {
-    let {classList} = this.$input
-    this.isComma = [...classList].indexOf('comma') > -1
-
-    Object.assign(this.eventHandler, {
+    this.eventHandler = {
+      focus: e => {
+        e.target.classList[e.type === 'focus' ? 'add' : 'remove']('focus')
+      },
       number: e => {
-        let value = e.target.value.replace(/[^\d]+/g, '').replace(/(\d)(?=(?:\d{3})+(?!\d))/g,'$1')
+        let value = e.target.value.replace(/\D+/g, '').replace(/(\d)(?=(?:\d{3})+(?!\d))/g,'$1')
         e.target.value = this.isComma ? window.UTILS.numberFormat.comma(value) : value
-      }
-    })
-
-    this.$input.addEventListener('keydown', this.eventHandler.number)
-    this.$input.addEventListener('keyup', this.eventHandler.number)
-  }
-
-  tel () {
-    Object.assign(this.eventHandler, {
+      },
       tel: e => {
         e.target.value = window.UTILS.numberFormat.tel(e.target.value)
       }
-    })
+    }
 
-    this.$input.addEventListener('keydown', this.eventHandler.tel)
-    this.$input.addEventListener('keyup', this.eventHandler.tel)
+    this.$input.addEventListener('focus', this.eventHandler.focus)
+    this.$input.addEventListener('blur', this.eventHandler.focus)
+
+    let {classList} = this.$input
+    this.isComma = [...classList].indexOf('comma') > -1
+
+    if ([...classList].indexOf('number') > -1) {
+      this.$input.addEventListener('keydown', this.eventHandler.number)
+      this.$input.addEventListener('keyup', this.eventHandler.number)
+
+    } else if ([...classList].indexOf('tel') > -1) {
+      this.$input.addEventListener('keydown', this.eventHandler.tel)
+      this.$input.addEventListener('keyup', this.eventHandler.tel)
+    }
   }
 
   clear () {
-    this.$input.removeEventListener('change', this.eventHandler.checkbox)
-    this.$input.removeEventListener('keydown', this.eventHandler.tel)
-    this.$input.removeEventListener('keyup', this.eventHandler.tel)
-    this.$input.removeEventListener('keydown', this.eventHandler.number)
-    this.$input.removeEventListener('keyup', this.eventHandler.number)
+    this.$input.classList.remove('focus')
     this.$input.removeEventListener('focus', this.eventHandler.focus)
     this.$input.removeEventListener('blur', this.eventHandler.focus)
+    this.$input.removeEventListener('keydown', this.eventHandler.number)
+    this.$input.removeEventListener('keyup', this.eventHandler.number)
+    this.$input.removeEventListener('keydown', this.eventHandler.tel)
+    this.$input.removeEventListener('keyup', this.eventHandler.tel)
+  }
+}
+
+class Checkbox extends Input {
+  constructor (el, options) {
+    super(el, options)
+
+    this.eventHandler = {
+      checkbox: e => e.target.setAttribute('checked', e.target.checked)
+    }
+
+    this.$input.addEventListener('change', this.eventHandler.checkbox)
+
+    let checked = this.$input.getAttribute('checked')
+    let isCheckedFalse =  checked === 'false' || checked === null || checked === undefined
+
+    this.$input.setAttribute('checked', isCheckedFalse ? 'false' : 'true')
+    this.$input.checked = isCheckedFalse ? false : true
+  }
+
+  clear () {
+    this.$input.removeAttribute('checked')
+    this.$input.removeEventListener('change', this.eventHandler.checkbox)
+  }
+}
+
+class Radio extends Input {
+  constructor (el, options) {
+    super(el, options)
   }
 }
 /** ****************************************************************************************************************** */
