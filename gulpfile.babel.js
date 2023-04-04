e2b63b4edc071c75afbb8bc95aac3815b295e5ae
ksg <ryuzuka1222>
2023-04-04 16:34:22 +0900

    script, style, libs 삭제

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,21 +6,19 @@ import del from 'del'
 import browserSync from 'browser-sync'
 import concat from 'gulp-concat'
 import bro from 'gulp-bro'
-import babelify from 'babelify'
 import uglify from 'gulp-uglify'
+import babelify from 'babelify'
 
 /**
- * node version: 18.15.0(lts/hydrogen)
+ * node version: 18.15.0 (lts/hydrogen)
  */
 
 const app = 'app'
 const src = 'src'
 const dist = `dist/${app}`
 const paths = {
-  script: [`${src}/**/*.js`, `!${src}/js/plugin/*.js`, `!${src}/lib/*.js`],
-  lib: [`${src}/lib/*.js`],
-  plugin: [`${src}/js/plugin/*.js`],
-  scss: `${src}/**/*.{css,scss}`,
+  script: [`${src}/**/*.js`],
+  style: [`${src}/**/*.{css,scss}`],
   html: `${src}/**/*.html`,
   image: `${src}/**/*.{png,jpg,jpeg,gif,svg,ico,mp4}`,
   font: `${src}/**/*.{ttf,otf,woff,woff2,eot,svg}`,
@@ -50,44 +48,27 @@ function htmlInclude() {
   .pipe(browserSync.stream())
 }
 
-function style() {
-  return gulp.src(paths.scss, {sourcemaps: true})
-    .pipe(sass(scssOptions).on('error', sass.logError))
-    .pipe(base64({
-      extensions: ['svg', 'png', /\.jpg#datauri$/i],
-      maxImageSize: 1024,
-      debug: true
-    }))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(dist, {sourcemaps: '/maps'}))
-    .pipe(browserSync.stream())
-}
-
-function lib() {
-  return gulp.src(paths.lib, { sourcemaps: true }).pipe(gulp.dest(dist+'/lib/'))
-}
-
-function plugin() {
-  return gulp.src(paths.plugin, { sourcemaps: true })
-    .pipe(bro({
-      // transform: [
-      //   babelify.configure({ presets: ['@babel/preset-env'] })
-      // ]
-    }))
-    // .pipe(uglify({toplevel: true}))
-    .pipe(concat('plugin.js'))
-    .pipe(gulp.dest(dist+'/js/'))
-}
-
 function script() {
-  return gulp.src(paths.script, {sourcemaps: true})
-  .pipe(bro({
-    // transform: [
-    //   babelify.configure({ presets: ['@babel/preset-env'] })
-    // ]
-  }))
+  return gulp.src(paths.script, {sourcemaps: true}).pipe(gulp.dest(dist))
   // .pipe(uglify({toplevel: true}))
-  .pipe(gulp.dest(dist))
+  // .pipe(bro({
+  //   transform: [
+  //     babelify.configure({ presets: ['@babel/preset-env'] })
+  //   ]
+  // }))
+}
+
+function style() {
+  return gulp.src(paths.style, {sourcemaps: true})
+  .pipe(sass(scssOptions).on('error', sass.logError))
+  .pipe(base64({
+    extensions: ['svg', 'png', /\.jpg#datauri$/i],
+    maxImageSize: 1024,
+    debug: true
+  }))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest(dist, {sourcemaps: '/maps'}))
+  .pipe(browserSync.stream())
 }
 
 function copyImage() {
@@ -114,17 +95,15 @@ function watchFiles(done) {
   })
   done()
   gulp.watch(paths.html, htmlInclude)
-  gulp.watch(paths.scss, style)
-  gulp.watch(paths.lib, lib)
-  gulp.watch(paths.plugin, plugin)
   gulp.watch(paths.script, script)
+  gulp.watch(paths.style, style)
   gulp.watch(paths.image, copyImage)
   gulp.watch(paths.font, copyFont)
   gulp.watch(paths.data, copyData)
 }
 
 const watch = gulp.parallel(watchFiles)
-const build = gulp.series(clean, gulp.parallel(htmlInclude, style, script, lib, plugin, copyImage, copyFont, copyData))
+const build = gulp.series(clean, gulp.parallel(htmlInclude, style, script, copyImage, copyFont, copyData))
 const dataBuild = gulp.parallel(copyImage, copyFont, copyData)
 
 // build
@@ -133,8 +112,6 @@ exports.build = build
 // dev
 export const dev = gulp.series([build, watch]);
 export const data = gulp.series([dataBuild])
-
-
 
 // exports.watch = watch
 // exports.build = build
